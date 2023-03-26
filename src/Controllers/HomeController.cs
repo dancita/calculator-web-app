ef6d8ac21f54b4c24b3fc0f367fc57a4dcc85cae
dancita <daniella.paal@gmail.com>
2023-03-26 14:29:05 +0100

    Initial commit

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using CalculatorWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculation calculation)
+        {
+            calculation.Result = calculation.FirstNumber + calculation.SecondNumber;
+            return View(calculation);
+        }
+    }
+}
---
+++ src/Models/Calculation.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorWebApp.Models
+{
+    public class Calculation
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public Operation? OperationType {get;set;}
+        public long? Result { get; set; }
+    }
+}
