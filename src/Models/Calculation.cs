04b7a3f672fe67f295b304349915efc7b9e39635
dancita <daniella.paal@gmail.com>
2023-03-27 20:14:29 +0100

    Refactoring, applying Bootstrap to the form, displaying radio buttons by iterating thorugh the enums

--- src/Models/Calculation.cs
+++ src/Models/Calculation.cs
@@ -1,10 +1,19 @@
-﻿namespace CalculatorWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CalculatorWebApp.Models
 {
     public class Calculation
     {
         public int FirstNumber { get; set; }
         public int SecondNumber { get; set; }
-        public Operation? OperationType {get;set;}
+        public OperationType? Operation {get;set;}
         public long? Result { get; set; }
+
+        public enum OperationType
+        {
+            Add,
+            Multiply
+        }
     }
 }
