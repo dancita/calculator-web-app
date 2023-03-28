3f46a82b13b1b318791577daf043083e2a0183ae
dancita <daniella.paal@gmail.com>
2023-03-28 17:55:18 +0100

    making radiobutton selection mandatory

--- src/Models/Calculation.cs
+++ src/Models/Calculation.cs
@@ -4,10 +4,15 @@ namespace CalculatorWebApp.Models
 {
     public class Calculation
     {
-       [Range(0, 1, ErrorMessage = "Number must be between 0 and 1")]
+        [Required]
+        [Range(0, 1, ErrorMessage = "Number must be between 0 and 1")]
         public long NumberA { get; set; }
+
+        [Required]
         [Range(0, 1, ErrorMessage = "Number must be between 0 and 1")]
         public long NumberB { get; set; }
+
+        [Required(ErrorMessage = "An operation must be selected")]
         public OperationType? Operation { get; set; }
         public long? Result { get; set; }
 
